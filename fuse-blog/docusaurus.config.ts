f167907f4018e6b0a77002187c0525db11bf7a02
kristof.muhi <kristof.muhi@gmail.com>
2025-02-23 21:14:03 +0100

    pathchanges

--- fuse-blog/docusaurus.config.ts
+++ fuse-blog/docusaurus.config.ts
@@ -10,10 +10,10 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https:/fuselock.org',
+  url: 'https://fuselock.org',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/index.html',
+  baseUrl: '/main/index.html',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
